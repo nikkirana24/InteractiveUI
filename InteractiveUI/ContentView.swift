1ec5a85d0f4ff163c162408c8b52215c28f2d9e7
Nikki <nikkiranabhat@gmail.com>
2025-07-23 16:58:46 -0400

    added buttons and text fields

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name: String = " "
+    @State private var textTitle = "What is your name?"
     var body: some View {
         VStack {
             Text("What is your name?")
                 .font(.title)
                 .fontWeight(.bold)
                 .foregroundColor(Color(hue: 1.0, saturation: 0.982, brightness: 0.478))
+            TextField("Type your name here...", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.black, width: 1)
+            Button("Submit Name") {
+            }
+            .font(.title3)
+            .buttonStyle(.borderedProminent)
+            .tint(Color(hue: 1.0, saturation: 0.982, brightness: 0.478))
         }
         .padding()
     }
