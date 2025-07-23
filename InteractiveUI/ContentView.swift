6a7cc4a81df3a19769e253689911061848866858
Nikki <nikkiranabhat@gmail.com>
2025-07-23 17:14:31 -0400

    added action button and more

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var textTitle = "What is your name?"
     var body: some View {
         VStack {
-            Text("What is your name?")
+            Text(textTitle)
                 .font(.title)
                 .fontWeight(.bold)
                 .foregroundColor(Color(hue: 1.0, saturation: 0.982, brightness: 0.478))
@@ -21,6 +21,7 @@ struct ContentView: View {
                 .font(.title)
                 .border(Color.black, width: 1)
             Button("Submit Name") {
+                textTitle = "Welcome, \(name)!"
             }
             .font(.title3)
             .buttonStyle(.borderedProminent)
